776fc57adc49c96e554e877ef99d9ac553a0ec6d
CherylEThomas <cherylelysethomas@hotmail.co.uk>
2022-07-21 10:10:06 +0000

    Postgres relational database set up for new Theme and User information to be stored in

--- brickbox/models.py
+++ brickbox/models.py
@@ -4,7 +4,7 @@ from brickbox import db
 class Theme(db.Model):
     # schema for the Theme model
     id = db.Column(db.Integer, primary_key=True)
-    theme_name = db.Column(db.String(25), unique=True, nullable=False)
+    theme_name = db.Column(db.String(30), unique=True, nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -14,8 +14,8 @@ class Theme(db.Model):
 class Users(db.Model):
     # schema for the Login model
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(260), nullable=False)
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
